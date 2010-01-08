6137e2a1bbeb2379a15cf0652ad211c4439ecf0e
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2010-01-08 09:03:27 +0000

    [gridkit] Some initial benchmark code

--- enhanced-incubator/benchmarks/patterns-benchmark/src/main/java/com/griddynamics/gridkit/coherence/patterns/command/benchmark/StatHelper.java
+++ enhanced-incubator/benchmarks/patterns-benchmark/src/main/java/com/griddynamics/gridkit/coherence/patterns/command/benchmark/StatHelper.java
@@ -16,19 +16,20 @@
 package com.griddynamics.gridkit.coherence.patterns.command.benchmark;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class StatHelper {
-
+	
 	public static void reportStatsMs(Map<Long, ExecMark> stats) {
 		if (stats == null || stats.isEmpty()) {
-			System.out.println("No data");
+			TestHelper.sysout("No data");
 		}
 		
 		long firstTime = stats.values().iterator().next().submitMs;
 		long lastTime = stats.values().iterator().next().execMs;
 
 		long cummulativeTime = 0;
-		long cummulativeSquareTime = 0;
+		double cummulativeSquareDev = 0;
 		long eventCount = 0;
 		long avgLatency = lastTime - firstTime;
 		long maxLatency = avgLatency;
@@ -53,7 +54,7 @@ public class StatHelper {
 
 			// Calc total time out
 			cummulativeTime += latency;
-			cummulativeSquareTime += latency * latency;
+			cummulativeSquareDev += (latency - (cummulativeTime / eventCount)) * (latency - (cummulativeTime / eventCount));
 
 			// Get max timeout
 			if (maxLatency < latency) {
@@ -85,14 +86,91 @@ public class StatHelper {
 		double scale = 1d; 
 		
 		// Calc standard deviation
-		final double stdDev = Math.sqrt(Math.abs(cummulativeSquareTime - cummulativeTime * cummulativeTime));
+		final double stdDev = Math.sqrt(Math.abs(cummulativeSquareDev));
 
-		System.out.println("Total time: " + (scale * (lastTime - firstTime)) / 1000 + "s");
-		System.out.println("Throughput: " + (eventCount * 1000) / (scale * (lastTime - firstTime)) + "op/s");
-		System.out.println("Average latency: " + scale * avgLatency + "ms");
-		System.out.println("Latency variance: " + scale * avgVariance + "ms");
-		System.out.println("Latency stddev: " + scale * stdDev + "ms");
-		System.out.println("Max latency: " + scale * maxLatency + "ms");
-		System.out.println("Min latency: " + scale * minLatency + "ms");
+		TestHelper.sysout("Total time [s]:        %014.12f" , (scale * (lastTime - firstTime)) / 1000);
+		TestHelper.sysout("Throughput [op/s]:     %014.12f" , (eventCount * 1000) / (scale * (lastTime - firstTime)));
+		TestHelper.sysout("Average latency [ms]:  %014.12f" , scale * avgLatency);
+		TestHelper.sysout("Latency variance [ms]: %014.12f" , scale * avgVariance);
+//		TestHelper.sysout("Latency stddev /[ms]:   %014.12f" , scale * stdDev);
+		TestHelper.sysout("Max latency [ms]:      %014.12f" , scale * maxLatency);
+		TestHelper.sysout("Min latency [ms]:      %014.12f" , scale * minLatency);
+	}	
+
+	public static void reportStatsNs(Map<Long, ExecMark> stats) {
+		if (stats == null || stats.isEmpty()) {
+			TestHelper.sysout("No data");
+		}
+		
+		long firstTime = stats.values().iterator().next().submitNs;
+		long lastTime = stats.values().iterator().next().execNs;
+		
+		long cummulativeTime = 0;
+		double cummulativeSquareDev = 0;
+		long eventCount = 0;
+		long avgLatency = lastTime - firstTime;
+		long maxLatency = avgLatency;
+		long minLatency = avgLatency;
+		
+		for (ExecMark execMark : stats.values()) {
+			
+			eventCount++;
+			
+			long submitTime = execMark.submitNs;
+			long execTime = execMark.execNs;
+			
+			if (lastTime < execTime) {
+				lastTime = execTime;
+			}
+			
+			if (firstTime > submitTime) {
+				firstTime = submitTime;
+			}
+			
+			long latency = execTime - submitTime;
+			
+			// Calc total time out
+			cummulativeTime += latency;
+			cummulativeSquareDev += (latency - (cummulativeTime / eventCount)) * (latency - (cummulativeTime / eventCount));
+			
+			// Get max timeout
+			if (maxLatency < latency) {
+				maxLatency = latency;
+			}
+			
+			// Get min timeout
+			if (minLatency > latency) {
+				minLatency = latency;
+			}
+		}
+		
+		// Calc avg
+		avgLatency = cummulativeTime / eventCount;
+		// Calc variance
+		long cummulativeVariance = 0;
+		for (ExecMark execMark : stats.values()) {
+			
+			long submitTime = execMark.submitNs;
+			long execTime = execMark.execNs;
+			long latency = execTime - submitTime;
+			
+			long var = Math.abs(avgLatency - latency);
+			cummulativeVariance += var;
+		}
+		
+		long avgVariance = cummulativeVariance / eventCount;
+		
+		double scale = 1d / TimeUnit.MILLISECONDS.toNanos(1); 
+		
+		// Calc standard deviation
+		final double stdDev = Math.sqrt(Math.abs(cummulativeSquareDev));
+		
+		TestHelper.sysout("Total time [s]:        %014.12f" , (scale * (lastTime - firstTime)) / 1000);
+		TestHelper.sysout("Throughput [op/s]:     %014.12f" , (eventCount * 1000) / (scale * (lastTime - firstTime)));
+		TestHelper.sysout("Average latency [ms]:  %014.12f" , scale * avgLatency);
+		TestHelper.sysout("Latency variance [ms]: %014.12f" , scale * avgVariance);
+//		TestHelper.sysout("Latency stddev /[ms]:   %014.12f" , scale * stdDev);
+		TestHelper.sysout("Max latency [ms]:      %014.12f" , scale * maxLatency);
+		TestHelper.sysout("Min latency [ms]:      %014.12f" , scale * minLatency);
 	}	
 }
