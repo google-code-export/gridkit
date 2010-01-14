eb1fc19326f18c41b62df4b9f5c70231337690c4
artem.panasyuk <artem.panasyuk@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2010-01-14 10:39:25 +0000

    [coherence-command-benchmark] separate CSVHelper class added

--- branches/coherence/enhanced-incubator/benchmarks/patterns-benchmark/src/main/java/com/griddynamics/gridkit/coherence/patterns/command/benchmark/StatHelper.java
+++ branches/coherence/enhanced-incubator/benchmarks/patterns-benchmark/src/main/java/com/griddynamics/gridkit/coherence/patterns/command/benchmark/StatHelper.java
@@ -15,95 +15,11 @@
  */
 package com.griddynamics.gridkit.coherence.patterns.command.benchmark;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class StatHelper
 {
-	static final class StatsCSVRow
-	{
-		public final int executionOrder;
-		public final CommandBenchmarkParams params;
-		public final BenchmarkStats stats;
-		public final TimeUnit counterType;
-		
-		public StatsCSVRow(int executionOrder, CommandBenchmarkParams params, BenchmarkStats stats, TimeUnit counterType)
-		{
-			this.executionOrder = executionOrder;
-			this.params = params;
-			this.stats = stats;
-			this.counterType = counterType;
-		}
-
-		public String toCSVRow()
-		{
-			return executionOrder + ";" +
-			    params.toCSVRow() + ";" +
-			     stats.toCSVRow() + ";" +
-		   counterType.toString();
-		}
-		
-		public static String getCSVHeader()
-		{
-			return "ExecutionOrder;" + CommandBenchmarkParams.getCSVHeader() + ";" +
-				   BenchmarkStats.getCSVHeader() + ";" + "CounterType";
-		}
-	}
-	
-	public static boolean storeResultsInCSV(String fileName, Collection<StatsCSVRow> items)
-	{
-		File file = new File(fileName);
-        
-		FileOutputStream fos;
-		try
-		{
-			fos = new FileOutputStream(file);
-		}
-		catch(FileNotFoundException e)
-		{
-			return false;
-		}
-		
-        BufferedWriter out;
-		try
-		{
-			out = new BufferedWriter(new OutputStreamWriter(fos,"ansi-1251"));
-		}
-		catch (UnsupportedEncodingException e)
-		{
-			try { fos.close(); } catch (IOException ioIgnored) {}
-			return false;
-		}
-       
-        try
-        {
-			out.write(StatsCSVRow.getCSVHeader() + "\n");
-			
-			for (StatsCSVRow r : items)
-			{
-				out.write(r.toCSVRow() + "\n");
-			}
-		}
-        catch (IOException e)
-        {
-			return false;
-		}
-        finally
-        {
-        	try { out.close(); } catch (IOException ioIgnored) {}
-        }
-       
-        return true;
-	}
-	
 	static interface TimeExtractor
 	{
 		long getSubmitTime(ExecMark ts);
