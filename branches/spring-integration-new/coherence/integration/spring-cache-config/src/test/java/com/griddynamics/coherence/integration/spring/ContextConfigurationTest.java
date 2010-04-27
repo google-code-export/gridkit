ddd9f293d3144b382a021f0fb6a00a30cc7e8d96
dmitri.lb <dmitri.lb@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2010-04-27 16:55:58 +0000

    map listener test

--- branches/spring-integration-new/coherence/integration/spring-cache-config/src/test/java/com/griddynamics/coherence/integration/spring/ContextConfigurationTest.java
+++ branches/spring-integration-new/coherence/integration/spring-cache-config/src/test/java/com/griddynamics/coherence/integration/spring/ContextConfigurationTest.java
@@ -2,17 +2,20 @@ package com.griddynamics.coherence.integration.spring;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.tangosol.net.NamedCache;
+import com.tangosol.util.MapEvent;
 
 /**
  * @author Dmitri Babaev
  */
-public class CacheStoreTest {
+public class ContextConfigurationTest {
 	private ApplicationContext applicationContext;
 
 	@Before
@@ -29,5 +32,24 @@ public class CacheStoreTest {
 		
 		cache.put("aaa", "4232");
 		assertEquals("4232", cache.get("aaa"));
+	}
+	
+	@Test
+	public void mapListener() {
+		NamedCache cache = applicationContext.getBean("simpleDistributedCache", NamedCache.class);
+		List<?> events = applicationContext.getBean("cacheEvents", List.class);
+		cache.clear();
+		events.clear();
+		
+		cache.put("aaa", "4232");
+		cache.put("aaa", "asd1");
+		
+		assertEquals(2, events.size());
+		assertEquals(((MapEvent)events.get(0)).getKey(), "aaa");
+		assertEquals(((MapEvent)events.get(0)).getNewValue(), "4232");
+		
+		assertEquals(((MapEvent)events.get(1)).getKey(), "aaa");
+		assertEquals(((MapEvent)events.get(1)).getOldValue(), "4232");
+		assertEquals(((MapEvent)events.get(1)).getNewValue(), "asd1");
 	}
 }
