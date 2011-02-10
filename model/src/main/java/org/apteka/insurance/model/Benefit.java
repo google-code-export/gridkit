9cbab33b0684da409ce07ebbc76c8a219296c2a5
artem.panasyuk@gmail.com <artem.panasyuk@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2011-02-10 00:47:54 +0000

    apteka fixes

--- model/src/main/java/org/apteka/insurance/model/Benefit.java
+++ model/src/main/java/org/apteka/insurance/model/Benefit.java
@@ -2,8 +2,12 @@ package org.apteka.insurance.model;
 
 import java.util.Map;
 
+import org.apteka.insurance.attribute.annotation.AttrToDict;
+
 public interface Benefit {
+	@AttrToDict
 	Map<String, Integer> getDrugLimits();
 	
+	@AttrToDict
 	Double getDiscretionValue();
 }
--- model/src/main/java/org/apteka/insurance/model/Drug.java
+++ model/src/main/java/org/apteka/insurance/model/Drug.java
@@ -1,6 +1,11 @@
 package org.apteka.insurance.model;
 
+import org.apteka.insurance.attribute.annotation.AttrToDict;
+
 public interface Drug {
+	@AttrToDict
 	String getName();
+	
+	@AttrToDict
 	Double getPrice();
 }
