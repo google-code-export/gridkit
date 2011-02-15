ff14acfa00f4e2750fbb347518380f20e2d88eb4
artem.panasyuk@gmail.com <artem.panasyuk@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2011-02-15 22:18:28 +0000

    temporary commit

--- branches/apteka/model/src/main/java/com/medx/type/TypeRegistry.java
+++ branches/apteka/model/src/main/java/com/medx/type/TypeRegistry.java
@@ -2,5 +2,8 @@ package com.medx.type;
 
 public interface TypeRegistry {
 	Class<?> getType(int id);
+	Class<?> getType(String name);
+	
 	int getTypeId(Class<?> clazz);
+	String getTypeName(Class<?> clazz);
 }
