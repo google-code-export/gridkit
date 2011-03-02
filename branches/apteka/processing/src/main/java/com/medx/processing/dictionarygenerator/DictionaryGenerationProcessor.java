d5c24246b19787c2238cc9357a0fdf515b22224e
artem.panasyuk@gmail.com <artem.panasyuk@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2011-03-02 12:26:05 +0000

    new annotation processor

--- branches/apteka/processing/src/main/java/com/medx/processing/dictionarygenerator/DictionaryGenerationProcessor.java
+++ branches/apteka/processing/src/main/java/com/medx/processing/dictionarygenerator/DictionaryGenerationProcessor.java
@@ -12,18 +12,11 @@ import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic.Kind;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @SupportedAnnotationTypes("com.medx.framework.annotation.ModelPackage")
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
-public class DictionaryGenerationProcessor extends AbstractProcessor {
-	private static Logger log = LoggerFactory.getLogger(DictionaryGenerationProcessor.class);
-	
+public class DictionaryGenerationProcessor extends AbstractProcessor {	
 	@Override
 	public boolean process(Set<? extends TypeElement> elements, RoundEnvironment environment) {
-		log.info("+++++++++++++++++++++++++++++++++++++++ strated size = " + elements.size());
-		
 		for (TypeElement modelPackage : elements) {
 			for (Element packet : environment.getElementsAnnotatedWith(modelPackage)) {
 				ModelPackageProcessor modelPackageProcessor = new ModelPackageProcessor((PackageElement)packet, environment);
