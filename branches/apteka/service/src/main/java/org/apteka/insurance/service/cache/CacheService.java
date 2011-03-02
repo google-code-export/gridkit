4cac07a5ce8627e011862d4730f3b288527225cc
artem.panasyuk@gmail.com <artem.panasyuk@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2011-03-02 15:36:48 +0000

    poms refactoring

--- branches/apteka/service/src/main/java/org/apteka/insurance/service/cache/CacheService.java
+++
@@ -1,7 +0,0 @@
-package org.apteka.insurance.service.cache;
-
-import java.util.concurrent.ConcurrentMap;
-
-public interface CacheService {
-	ConcurrentMap<Object, Object> getCache(String name);
-}
