16a4019e66b6c99a0f6e09d4394ec3031e20433d
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2011-03-09 04:06:15 +0000

    Bean interface

---
+++ branches/apteka/framework/src/main/java/com/medx/framework/bean/Bean.java
@@ -0,0 +1,22 @@
+package com.medx.framework.bean;
+
+import java.util.Map;
+
+import com.medx.framework.metadata.AttrKey;
+
+public interface Bean {
+	
+	public <T> T cast(Class<T> type);
+
+	public boolean isInstanceOf(Class<?> type);
+
+	public void addType(Class<?> type);
+	
+	public void remoteType(Class<?> type);
+	
+	public <V> V getAttribute(AttrKey<V> key);
+
+	public <V> void setAttribute(AttrKey<V> key, V value);
+		
+	public Map<Integer, Object> asMap();
+}
