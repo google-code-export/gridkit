e2eeb5d219c8ae29bc848f3f3fe138e646e6b00c
dmitri.lb <dmitri.lb@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2011-11-05 19:26:52 +0000

    batch-uploader module is renamed to common tools

--- coherence-tools/trunk/common-tools/src/main/java/org/gridkit/coherence/store/BatchStoreUploader.java
+++ coherence-tools/trunk/common-tools/src/main/java/org/gridkit/coherence/store/BatchStoreUploader.java
@@ -10,6 +10,13 @@ import com.tangosol.util.Binary;
 import com.tangosol.util.Converter;
 import com.tangosol.util.UID;
 
+/**
+ * A tool to upload a bulk of entries to the cache with synchronious write to the underlaying storage.
+ * This is done by issinig a set of per-node entry processors which performs a per-partition batch put operation directly to the backing map.
+ * This will work for for write-through backing maps.
+ *
+ * @author Dmitri Babaev
+ */
 public class BatchStoreUploader {
 	private int perMemberPutBatchSize;
 
