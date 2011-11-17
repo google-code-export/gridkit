afd9c3b451f53ebd6c4cb4f6a6896c8cf7c6dd62
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2011-11-17 15:11:08 +0000

    [fluent DSL] ...

--- branches/aragozin-sandbox/fluent-grammar/src/main/java/org/gridkit/flugram/AstNodeHandler.java
+++ branches/aragozin-sandbox/fluent-grammar/src/main/java/org/gridkit/flugram/AstNodeHandler.java
@@ -3,9 +3,4 @@ package org.gridkit.flugram;
 
 public interface AstNodeHandler<T> {
 
-	/**
-	 * Evaluates AST node and returns produced object.
-	 * Call to this method terminates node life cycle.
-	 */
-	public T evaluate();
 }
