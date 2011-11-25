4a2b4fbdb4b2c586282d3b9534d7da133a21b8ca
dmitri.lb <dmitri.lb@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2011-11-25 13:34:07 +0000

    using new isolate tools

--- coherence-tools/trunk/common-tools/src/test/java/org/gridkit/coherence/store/ReplicationCheckApp.java
+++ coherence-tools/trunk/common-tools/src/test/java/org/gridkit/coherence/store/ReplicationCheckApp.java
@@ -1,65 +1,48 @@
 package org.gridkit.coherence.store;
 
-import org.gridkit.coherence.util.classloader.Isolate;
-import org.gridkit.coherence.util.classloader.NodeActions;
+import org.gridkit.utils.vicluster.CohHelper;
+import org.gridkit.utils.vicluster.ViCluster;
 
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.NamedCache;
 
 public class ReplicationCheckApp {
-	
-	public static class DataUpload implements Runnable {
-		@Override
-		public void run() {
-			NamedCache cache = CacheFactory.getCache("store-cache");
-			
-			BatchStoreUploader uploader = new BatchStoreUploader(cache);
-			uploader.put("1", "test");
-			uploader.put("2", "test2");
-			uploader.flush();
-		}
-	}
-	
-	public static class DataRead implements Runnable {
-		@Override
-		public void run() {
-			NamedCache cache = CacheFactory.getCache("store-cache");
-			
-			System.out.println(cache.get("1"));
-			System.out.println(cache.get("2"));
-		}
-	}
-	
 	public static void main(String[] args) {
-		Isolate storage1 = new Isolate("storage-node-1", "com.tangosol", "org.gridkit");
-		storage1.start();
-        Isolate storage2 = new Isolate("storage-node-2", "com.tangosol", "org.gridkit");
-        storage2.start();
-        Isolate client = new Isolate("client", "com.tangosol", "org.gridkit");
-        client.start();
+	    ViCluster cluster = new ViCluster("replicationCheck", "com.tangosol", "org.gridkit");
+        CohHelper.enableFastLocalCluster(cluster);
+        CohHelper.enableJmx(cluster);
+        
+        cluster.setProp("org.gridkit.batch-store-uploader.partition-count", "1");
+        cluster.setProp("tangosol.coherence.cacheconfig", "batch-uploader-test-cache-config.xml");
+        
+        CohHelper.localstorage(cluster.node("client"), false);
+        
+        String cacheService = cluster.node("client").getServiceNameForCache("store-cache"); 
+        
+        cluster.node("storage1").getService(cacheService);
+        
+        cluster.node("client").exec(new Runnable() {
+            @Override
+            public void run() {
+                NamedCache cache = CacheFactory.getCache("store-cache");
+                BatchStoreUploader uploader = new BatchStoreUploader(cache);
+                uploader.put("1", "test");
+                uploader.put("2", "test2");
+                uploader.flush();
+            }
+        });
 
-        System.setProperty("org.gridkit.batch-store-uploader.partition-count", "1");
-        storage1.submit(NodeActions.Start.class, "batch-uploader-test-cache-config.xml");
-        storage1.submit(NodeActions.GetService.class, "DistributedStore");
         
-        System.setProperty("tangosol.coherence.distributed.localstorage", "false");
-        client.submit(NodeActions.Start.class, "batch-uploader-test-cache-config.xml");
-        client.submit(DataUpload.class);
+        cluster.node("storage2").getService(cacheService);
+        
+        CohHelper.jmxWaitForStatusHA(cluster.node("storage2"), cacheService, "NODE-SAFE");
 
-        System.setProperty("tangosol.coherence.distributed.localstorage", "true");
-        storage2.submit(NodeActions.Start.class, "batch-uploader-test-cache-config.xml");
-        storage2.submit(NodeActions.GetService.class, "DistributedStore");
+        cluster.node("storage1").kill();
         
-        try {
-			Thread.sleep(10000); //waiting for partition backup to be copied to the second node
-		} catch (InterruptedException e) {
-		}
+        NamedCache cache = cluster.node("client").getCache("store-cache");
+        System.out.println(cache.get("1"));
+        System.out.println(cache.get("2"));
         
-        storage1.submit(NodeActions.Crash.class, 0);
-        
-        client.submit(DataRead.class);
-        
-        client.stop();
-        storage2.stop();
+        cluster.shutdown();
 	}
 }
