9c18d097dffd2ec186de5dfafe7b36a058756e25
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2012-06-14 12:54:34 +0000

    Generic marshaling support for anonymous classes

--- lucene-x/grid-lab/src/main/java/org/gridkit/util/vicontrol/marshal/Marshaled.java
+++ lucene-x/grid-lab/src/main/java/org/gridkit/util/vicontrol/marshal/Marshaled.java
@@ -1,9 +1,10 @@
 package org.gridkit.util.vicontrol.marshal;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 public interface Marshaled extends Serializable {
 	
-	public Object unmarshal();
+	public Object unmarshal() throws IOException;
 	
 }
