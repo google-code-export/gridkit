af58576b2c65a4a78577fafdc6286a50c66097e0
artem.panasyuk@gmail.com <artem.panasyuk@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2012-10-02 19:50:42 +0000

    printing scratch

---
+++ branches/nimble/src/main/java/org/gridkit/nimble/print/TablePrinter.java
@@ -0,0 +1,114 @@
+package org.gridkit.nimble.print;
+
+import java.io.PrintStream;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.gridkit.nimble.util.Pair;
+
+public abstract class TablePrinter {
+    private DecimalFormat decFormat = new DecimalFormat("0.00");
+    private boolean sortColumns = false;
+    private Object nullValue = "";
+    
+    protected abstract void print(PrintStream stream, List<List<Object>> table);
+    
+    public void print(PrintStream stream, LinePrinter linePrinter) {
+        TablePrinterContetx context = new TablePrinterContetx();
+
+        linePrinter.print(context);
+        
+        Pair<Set<String>, List<Map<String,Object>>> data = context.finish();
+        
+        List<List<Object>> table = new ArrayList<List<Object>>();
+        
+        table.add(new ArrayList<Object>(data.getA()));
+        
+        for (Map<String,Object> mapRow : data.getB()) {
+            List<Object> listRow = new ArrayList<Object>();
+            
+            for (String column : data.getA()) {
+                listRow.add(mapRow.get(column));
+            }
+            
+            table.add(listRow);
+        }
+        
+        print(stream, table);
+    }
+    
+    private class TablePrinterContetx implements LinePrinter.Contetx {
+        private Set<String> columns = newColumnsSet();
+        private List<Map<String,Object>> data = new ArrayList<Map<String,Object>>();
+        
+        private Map<String,Object> curLine = new HashMap<String, Object>();
+        private Set<String> curColumns = newColumnsSet();
+
+        @Override
+        public void newline() {
+            data.add(curLine);
+            columns.addAll(curColumns);
+            
+            curLine = new HashMap<String, Object>();
+            curColumns = newColumnsSet();
+        }
+        
+        @Override
+        public void cell(String name, Object object) {
+            curColumns.add(name);
+            curLine.put(name, object);
+        }
+        
+        public Pair<Set<String>, List<Map<String,Object>>> finish() {
+            if (!curColumns.isEmpty()) {
+                newline();
+            }
+            
+            return Pair.newPair(columns, data);
+        }
+    }
+    
+    private Set<String> newColumnsSet() {
+        return sortColumns ? new TreeSet<String>() : new LinkedHashSet<String>();
+    }
+    
+    protected String toString(Object value) {
+        if (value == null) {
+            return toString(nullValue);
+        } else if (value instanceof Double || value instanceof Float) {
+            return decFormat.format(value);
+        } else {
+            return value.toString();
+        }
+    }
+
+    public DecimalFormat getDecFormat() {
+        return decFormat;
+    }
+
+    public void setDecFormat(DecimalFormat decFormat) {
+        this.decFormat = decFormat;
+    }
+
+    public boolean isSortColumns() {
+        return sortColumns;
+    }
+
+    public void setSortColumns(boolean sortColumns) {
+        this.sortColumns = sortColumns;
+    }
+
+    public Object getNullValue() {
+        return nullValue;
+    }
+
+    public void setNullValue(Object nullValue) {
+        this.nullValue = nullValue;
+    }
+}
