d8a8068105413364561c10856cc9956ca9f48285
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2012-11-02 06:32:22 +0000

    [maven-release-plugin]  copy for tag vicluster-parent-0.7.7

---
+++ grid-lab/tags/vicluster-parent-0.7.7/zerormi/src/main/java/org/gridkit/zerormi/IOHelper.java
@@ -0,0 +1,31 @@
+package org.gridkit.zerormi;
+
+import java.io.EOFException;
+import java.net.SocketException;
+
+
+class IOHelper {
+	
+	public static boolean isSocketTerminationException(Exception e) {
+		if (e instanceof EOFException) {
+			return true;
+		}
+		else if (e.getClass() == java.io.IOException.class) {
+			if ("pipe closed".equals(e.getMessage().toLowerCase())) {
+				return true;
+			}
+		}
+		else if (e instanceof SocketException) {
+			if ("connection reset".equals(e.getMessage().toLowerCase())) {
+				return true;
+			}
+			if ("socket closed".equals(e.getMessage().toLowerCase())) {
+				return true;
+			}
+		}
+		
+		// otherwise
+		return false;
+	}
+
+}
