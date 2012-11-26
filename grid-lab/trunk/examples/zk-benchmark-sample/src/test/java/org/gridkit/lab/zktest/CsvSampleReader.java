bd9842bfcfa9f679bc6af5d2093debfc82300d47
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2012-11-26 13:56:08 +0000

    [nimble] ZooKeeper benchmark tutorial - tweaking reporting

---
+++ grid-lab/trunk/examples/zk-benchmark-sample/src/test/java/org/gridkit/lab/zktest/CsvSampleReader.java
@@ -0,0 +1,128 @@
+package org.gridkit.lab.zktest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.gridkit.nimble.metering.SampleReader;
+
+public class CsvSampleReader implements SampleReader {
+
+	private BufferedReader reader;
+	
+	private List<Object> header;
+	private Map<Object, Object> row;
+	
+	public CsvSampleReader(Reader reader) throws IOException {
+		this.reader = toBuffered(reader);
+		readHeader();
+	}
+	
+	private static BufferedReader toBuffered(Reader reader) {
+		if (reader instanceof BufferedReader) {
+			return (BufferedReader)reader;
+		}
+		else {
+			return new BufferedReader(reader);
+		}
+	}
+	
+	private void readHeader() throws IOException {
+		String[] line = readLine();
+		if (line == null) {
+			throw new IOException("File is empty");
+		}
+		if (line[0].equals("sep=")) {
+			readHeader();
+			return;
+		}
+		header = new ArrayList<Object>(line.length);
+		for(String column: line) {
+			column = column.trim();
+			header.add(column);
+		}
+	}
+
+	
+	@Override
+	public boolean isReady() {
+		return row != null;
+	}
+
+	@Override
+	public boolean next() {
+		try {
+			row = createRow(readLine());
+		} catch (IOException e) {
+			return false;
+		}
+		return row != null;
+	}
+
+	@Override
+	public List<Object> keySet() {
+		return header;
+	}
+
+	@Override
+	public Object get(Object key) {
+		return row.get(String.valueOf(key));
+	}
+
+	private Map<Object, Object> createRow(String[] line) {
+		if (line == null) {
+			return null;
+		}
+		else {
+			Map<Object, Object> map = new HashMap<Object, Object>();
+			for(int i = 0; i != line.length; ++i) {
+				Object h = header.get(i);
+				Object v = parse(line[i]);
+				map.put(h, v);
+			}
+			return map;
+		}
+	}
+	
+	private Object parse(String string) {
+		String v = string.trim();
+		if (v.length() == 0) {
+			return null;
+		}
+		else {
+			try {
+				return Integer.parseInt(v);
+			}
+			catch(NumberFormatException e) {
+				// ignore;
+			}
+			try {
+				return Long.parseLong(v);
+			}
+			catch(NumberFormatException e) {
+				// ignore;
+			}
+			try {
+				return Double.parseDouble(v);
+			}
+			catch(NumberFormatException e) {
+				// ignore;
+			}
+			return v;
+		}
+	}
+
+	private String[] readLine() throws IOException {
+		String line = reader.readLine();
+		if (line == null) {
+			return null;
+		}
+		else {
+			return line.split(";");
+		}
+	}	
+}
---
+++ grid-lab/trunk/examples/zk-benchmark-sample/src/test/java/org/gridkit/lab/zktest/DataSliceExtractor.java
@@ -0,0 +1,86 @@
+package org.gridkit.lab.zktest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.gridkit.nimble.metering.Measure;
+import org.gridkit.nimble.metering.SampleReader;
+import org.gridkit.nimble.pivot.Filters;
+import org.gridkit.nimble.pivot.SampleFilter;
+
+public class DataSliceExtractor {
+	
+	private List<SampleFilter> filters = new ArrayList<SampleFilter>();
+	private List<Object> extract = new ArrayList<Object>();
+	
+	private List<double[]> data = new ArrayList<double[]>();
+
+	public void filter(Object key, Object value) {
+		filters.add(Filters.equals(key, value));
+	}
+
+	public void filter(SampleFilter filter) {
+		filters.add(filter);
+	}
+	
+	public void feed(SampleReader reader) {
+		if (reader.isReady() || reader.next()) {
+			while(true) {
+				process(reader);
+				if (!reader.next()) {
+					break;
+				}
+			}
+		}
+	}
+	
+	public List<double[]> getData() {
+		return data;
+	}
+	
+	public void clear() {
+		data.clear();
+	}
+	
+	private void process(SampleReader reader) {
+		for(SampleFilter f: filters) {
+			if (!f.match(reader)) {
+				return;
+			}
+		}
+		double[] row = new double[extract.size()];
+		for(int i = 0; i != row.length; ++i) {
+			Object v = reader.get(extract.get(i));
+			row[i] = ((Number)v).doubleValue();
+		}
+		data.add(row);
+	}
+
+	public void addField(Object key) {
+		extract.add(key);
+	}
+	
+	public void sort(int n) {
+		Collections.sort(data, new DoubleArrayComparator(n));
+	}
+
+	private static class DoubleArrayComparator implements Comparator<double[]> {
+
+		private final int n;
+		
+		private DoubleArrayComparator(int n) {
+			this.n = n;
+		}
+
+		@Override
+		public int compare(double[] o1, double[] o2) {			
+			return Double.compare(o1[n], o2[n]);
+		}
+	}
+
+	public void extract(Object attrKey) {
+		extract.add(attrKey);
+	}	
+}
