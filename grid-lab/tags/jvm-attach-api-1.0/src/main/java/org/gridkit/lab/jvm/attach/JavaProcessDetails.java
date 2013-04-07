5e4fb6f80a1980a275da3ef5c023bc09952e283a
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2013-04-07 22:03:44 +0000

    [maven-release-plugin]  copy for tag jvm-attach-api-1.0

---
+++ grid-lab/tags/jvm-attach-api-1.0/src/main/java/org/gridkit/lab/jvm/attach/JavaProcessDetails.java
@@ -0,0 +1,21 @@
+package org.gridkit.lab.jvm.attach;
+
+import java.util.Properties;
+
+import javax.management.MBeanServerConnection;
+
+public interface JavaProcessDetails {
+	
+	public long getPid();
+	
+	public String getDescription();
+	
+	public JavaProcessId getJavaProcId();
+	
+	public Properties getSystemProperties();
+	
+	public Properties getAgentProperties();
+
+	public MBeanServerConnection getMBeans();
+
+}
