2a5a2cbbd8aaf8d3c85a35bb8639562cf5032dd5
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2013-05-24 15:09:58 +0000

    ...

---
+++ lucube/src/test/java/org/gridkit/lab/mcube/QueryGraphTest.java
@@ -0,0 +1,23 @@
+package org.gridkit.lab.mcube;
+
+import org.gridkit.lab.mcube.QueryGraph.RowSource;
+import org.junit.Test;
+
+public class QueryGraphTest {
+
+	
+	@Test
+	public void simple_graph_test() {
+		
+		// goal - query value which will output of statistical summary of field grouped by value of another attribute 
+		
+		QueryGraph graph = new QueryGraph();
+		
+		RowSource root = graph.getRootSource();
+		
+		
+		
+		
+		
+	}
+}
