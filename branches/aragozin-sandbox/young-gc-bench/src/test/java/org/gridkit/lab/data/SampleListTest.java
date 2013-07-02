b95b5e631748212b636d2e558e3fd422aa1b3081
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2013-07-02 15:14:13 +0000

    ...

--- branches/aragozin-sandbox/young-gc-bench/src/test/java/org/gridkit/lab/data/SampleListTest.java
+++ branches/aragozin-sandbox/young-gc-bench/src/test/java/org/gridkit/lab/data/SampleListTest.java
@@ -3,13 +3,7 @@ package org.gridkit.lab.data;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
