c2502c9bdd953f3a7d0927e41b51c1cc9b34fec1
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2013-07-03 13:11:16 +0000

    [maven-release-plugin]  copy for tag chtest-0.2.3

---
+++ coherence-tools/tags/chtest-0.2.3/src/test/java/example/Envelop.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright 2013 Alexey Ragozin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example;
+
+import java.io.IOException;
+
+import com.tangosol.io.pof.PofReader;
+import com.tangosol.io.pof.PofSerializer;
+import com.tangosol.io.pof.PofWriter;
+import com.tangosol.util.Binary;
+
+public class Envelop {
+
+	public static final int TIMESTAMP_POF 	=   1;
+	public static final int DELETED_POF   =   2;
+	public static final int PAYLOAD_POF 	=  20;
+	
+	protected long timestamp;
+	protected boolean deleted;
+    protected Object payload;
+    protected Binary binaryPayload;
+    transient boolean serverMode;
+
+    /** TO BE USED WITH SERIALIZER */
+    protected Envelop(long timestamp, boolean deleted, Object payload, Binary binaryPayload, boolean serverMode) {
+		this.timestamp = timestamp;
+		this.deleted = deleted;
+		this.payload = payload;
+		this.binaryPayload = binaryPayload;
+		this.serverMode = serverMode;
+	}
+
+    /** Constructor used on client side */
+	public Envelop(Object payload, long timestamp, boolean deleted) {
+        this.payload = payload;
+        this.timestamp = timestamp;
+        this.deleted = deleted;
+        this.serverMode = false;
+    }
+    
+    public Object getPayload() {
+        return payload;
+    }
+    
+    public Binary getBinaryPayload() {
+    	return binaryPayload;
+    }
+    
+    public long getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public static class ServerSerializer implements PofSerializer {
+
+		@Override
+		public Object deserialize(PofReader in) throws IOException {
+			long timestamp = in.readLong(TIMESTAMP_POF);
+			boolean deleted = in.readBoolean(DELETED_POF);
+			Binary data = in.readRemainder();			
+			Envelop dv = new Envelop(timestamp, deleted, null, data, true);
+			return dv;
+		}
+
+		@Override
+		public void serialize(PofWriter out, Object o) throws IOException {			
+			Envelop dv = (Envelop) o;
+			if (!dv.serverMode) {
+				throw new IllegalArgumentException("Object is in client mode, but server serializer is used. Something wrong with POF config!");
+			}
+			out.writeLong(TIMESTAMP_POF, dv.getTimestamp());
+			out.writeBoolean(DELETED_POF, dv.isDeleted());
+			out.writeRemainder(dv.getBinaryPayload());
+		}
+    }
+    
+    public static class ClientSerializer implements PofSerializer {
+
+		@Override
+		public Object deserialize(PofReader in) throws IOException {
+			long timestamp = in.readLong(TIMESTAMP_POF);
+			boolean deleted = in.readBoolean(DELETED_POF);
+			Object payload = in.readObject(PAYLOAD_POF);
+			Binary data = in.readRemainder();			
+			Envelop dv = new Envelop(timestamp, deleted, payload, data, false);
+			return dv;
+		}
+
+		@Override
+		public void serialize(PofWriter out, Object o) throws IOException {
+			Envelop dv = (Envelop) o;
+			if (dv.serverMode) {
+				throw new IllegalArgumentException("Envelop is in server mode, but client serializer is used. Something wrong with POF config!");
+			}
+			out.writeLong(TIMESTAMP_POF, dv.getTimestamp());
+			out.writeBoolean(DELETED_POF, dv.isDeleted());
+			out.writeObject(PAYLOAD_POF, dv.getPayload());
+			out.writeRemainder(dv.getBinaryPayload());
+		}    	
+    }
+}
---
+++ coherence-tools/tags/chtest-0.2.3/src/test/java/org/gridkit/coherence/test/rwbm/DecoratorEventCheck.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright 2013 Alexey Ragozin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gridkit.coherence.test.rwbm;
+
+import java.io.Serializable;
+import java.util.concurrent.Callable;
+
+import org.gridkit.coherence.chtest.CohCloud.CohNode;
+import org.gridkit.coherence.chtest.CohCloudRule;
+import org.gridkit.coherence.chtest.DisposableCohCloud;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
+import com.tangosol.util.MapEvent;
+import com.tangosol.util.MapListener;
+import com.tangosol.util.MapTrigger;
+import com.tangosol.util.filter.EqualsFilter;
+
+public class DecoratorEventCheck {
+
+	@Rule
+	public CohCloudRule cloud = new DisposableCohCloud();
+	
+	@Test
+	public void test_decorator_listener() {
+		final String cacheName = "write-behind-A";
+		
+		cloud.all().presetFastLocalCluster();
+		cloud.all().cacheConfig("/cache-store-cache-config.xml");
+		
+		CohNode storage = cloud.node("storage");
+		storage.localStorage(true);
+		
+		CohNode client = cloud.node("client");
+		client.localStorage(false);
+		
+		cloud.all().getCache(cacheName);
+		
+		client.exec(new Callable<Void>(){
+			@Override
+			public Void call() throws Exception {
+				
+				NamedCache cache = CacheFactory.getCache(cacheName);
+				cache.addMapListener(new EntryListener());
+				
+				cache.put("A", "aaaa");
+				System.out.println("A <- aaaa");
+				System.out.println("not-yet-stored " + cache.keySet(new EqualsFilter(new StoreFlagExtractor(), Boolean.FALSE)));
+				
+				Thread.sleep(5000);
+				System.out.println("not-yet-stored " + cache.keySet(new EqualsFilter(new StoreFlagExtractor(), Boolean.FALSE)));
+				
+				return null;
+			}
+		});
+	}
+	
+	@Test
+	public void test_decorator_index() {
+		final String cacheName = "write-behind-A";
+		
+		cloud.all().presetFastLocalCluster();
+		cloud.all().cacheConfig("/cache-store-cache-config.xml");
+		
+		CohNode storage = cloud.node("storage");
+		storage.localStorage(true);
+		
+		CohNode client = cloud.node("client");
+		client.localStorage(false);
+		
+		cloud.all().getCache(cacheName);
+		
+		client.exec(new Callable<Void>(){
+			@Override
+			public Void call() throws Exception {
+				
+				NamedCache cache = CacheFactory.getCache(cacheName);
+				
+//					for(int i = 0; i != 1000; ++i) {
+//						cache.put(i, i);
+//					}
+//					
+//					cache.addMapListener(new EntryListener("test"));
+				
+				cache.addIndex(new StoreFlagExtractor(), false, null);
+				
+				cache.put("A", "aaaa");
+				System.out.println("A <- aaaa");
+				System.out.println("not-yet-stored " + cache.keySet(new EqualsFilter(new StoreFlagExtractor(), Boolean.FALSE)));
+				System.out.println("...");
+				
+				Thread.sleep(5000);
+				System.out.println("...");
+				System.out.println("not-yet-stored " + cache.keySet(new EqualsFilter(new StoreFlagExtractor(), Boolean.FALSE)));
+			
+				return null;
+			}
+		});
+	}
+	
+	@SuppressWarnings("serial")
+	public static class EntryListener implements MapListener, Serializable {
+
+		private String marker = "default";
+		
+		public EntryListener() {
+			System.out.println("Creating default listener");
+		}
+		
+		public EntryListener(String marker) {
+			this.marker = marker;
+		}
+
+		@Override
+		public void entryInserted(MapEvent event) {
+			System.out.println(marker + ": " + event);
+		}
+
+		@Override
+		public void entryUpdated(MapEvent event) {
+			System.out.println(marker + ": " + event);
+		}
+
+		@Override
+		public void entryDeleted(MapEvent event) {
+			System.out.println(marker + ": " + event);
+		}
+	}
+	
+	@SuppressWarnings("serial")
+	public static class EntryTrigger implements MapTrigger, Serializable {
+
+		@SuppressWarnings("unused")
+		private String marker = "default";
+		
+		public EntryTrigger() {
+			System.out.println("Creating default trigger");
+		}
+		
+		public EntryTrigger(String marker) {
+			this.marker = marker;
+		}
+
+		@Override
+		public void process(Entry entry) {
+			if (entry.isOriginalPresent() && entry.isPresent()) {
+				entry.setValue(entry.getValue().toString() + "-X");
+			}
+		}
+	}	
+}
