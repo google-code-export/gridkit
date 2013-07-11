e9e651f5543079ce8ed9cd972b97388bd48ee82c
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2013-07-11 15:23:58 +0000

    ...

--- young-gc-bench/src/test/java/org/gridkit/lab/data/HistogramTest.java
+++ young-gc-bench/src/test/java/org/gridkit/lab/data/HistogramTest.java
@@ -44,6 +44,53 @@ public class HistogramTest {
 		bg.show();
 	}
 
+	
+	@Test
+	public void calculateMemAccess() throws InterruptedException {
+		
+		GraphBuilder bg = new GraphBuilder();
+		MemAccessBench bench64 = new MemAccessBench();
+		bench64.initUnallignedByteMode(new Random(0), 128 << 20, 100);
+
+		MemAccessBench bench256 = new MemAccessBench();
+		bench256.initUnallignedByteMode(new Random(0), 256 << 20, 100);
+
+		for(int i = 0; i != 1000; ++i) {
+			bench64.measure(100, new double[10000]);
+		}
+		System.gc();
+		System.gc();
+		System.gc();
+		Thread.sleep(1000);
+		
+		addMemAccessSerie(bg, "128 MiB, bytes, 1000, 10", bench64, 10);
+		addMemAccessSerie(bg, "256 MiB, bytes, 1000, 10", bench256, 10);
+		addMemAccessSerie(bg, "128 MiB, bytes, 1000, 10 [2]", bench64, 10);
+		addMemAccessSerie(bg, "256 MiB, bytes, 1000, 10 [2]", bench256, 10);
+		
+		bg.show();
+	}
+	
+	private int addMemAccessSerie(GraphBuilder gb, String name, MemAccessBench bench, int spree) {
+		int acc = 0;
+		int rs = 10000;
+		int nr = 20;
+		
+		DoubleQuantileFinder qfinder = DoubleQuantileFinderFactory.newDoubleQuantileFinder(true, rs * nr, 0.01, 1e-5, 100, new DRand());
+		double[] block = new double[rs];
+		
+		for(int i = 0; i != nr; ++i) {
+			acc += bench.measure(spree, block);
+			qfinder.addAllOf(new DoubleArrayList(block));			
+		}
+		
+		DoubleArrayList x = DoubleQuantileFinderFactory.newEquiDepthPhis(100);
+		DoubleArrayList y = qfinder.quantileElements(x);
+		gb.addSeries("main", name, x, y);
+		
+		return acc;
+	}
+	
 	private double sqr(double v) {
 		return v * v;
 	}
