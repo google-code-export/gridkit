ea1c2acc8e5d5d5a2617acda11feae65694d366b
artem.panasyuk@gmail.com <artem.panasyuk@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2013-11-17 15:29:30 +0000

    fixes

--- grid-lab/trunk/orchestrator/src/main/java/org/gridkit/lab/orchestration/Sleep.java
+++ grid-lab/trunk/orchestrator/src/main/java/org/gridkit/lab/orchestration/Sleep.java
@@ -7,16 +7,14 @@ import java.util.concurrent.TimeUnit;
 import org.gridkit.lab.orchestration.script.ScriptExecutor.Box;
 import org.gridkit.lab.orchestration.util.NamedThreadFactory;
 
-public class Sleep implements ExecutableAction, Runnable {
+public class Sleep implements ExecutableAction {
     private static ScheduledExecutorService EXECUTOR = Executors.newScheduledThreadPool(
         1, new NamedThreadFactory("Sleep-Executor", true)
     );
     
-    private final SourceRef ref;
-    private final long timeout;
-    private final TimeUnit unit;
-    
-    private Box box;
+    private SourceRef ref;
+    private long timeout;
+    private TimeUnit unit;
     
     public Sleep(SourceRef ref, long timeout, TimeUnit unit) {
         this.ref = ref;
@@ -26,17 +24,11 @@ public class Sleep implements ExecutableAction, Runnable {
 
     @Override
     public void execute(Box box) {
-        this.box = box;
         try {
-            EXECUTOR.schedule(this, timeout, unit);
+            EXECUTOR.schedule(new Executor(box), timeout, unit);
         } catch (RuntimeException e) {
             box.failure(e);
         }
-    }
-
-    @Override
-    public void run() {
-        box.success();
     }
 
     @Override
@@ -48,5 +40,17 @@ public class Sleep implements ExecutableAction, Runnable {
     public String toString() {
         return ref.toString();
     }
+    
+    private static class Executor implements Runnable {
+        private Box box;
 
+        public Executor(Box box) {
+            this.box = box;
+        }
+
+        @Override
+        public void run() {
+            box.success();            
+        }
+    }
 }
