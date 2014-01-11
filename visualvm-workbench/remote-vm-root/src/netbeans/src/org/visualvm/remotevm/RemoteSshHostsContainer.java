88c3c51fcd414452ee54d157a2fd6d09484934b4
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2014-01-11 23:15:52 +0000

    [remotevm] Minor cleanup

--- visualvm-workbench/remote-vm-root/src/netbeans/src/org/visualvm/remotevm/RemoteSshHostsContainer.java
+++ visualvm-workbench/remote-vm-root/src/netbeans/src/org/visualvm/remotevm/RemoteSshHostsContainer.java
@@ -1,6 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2012-2014 Alexey Ragozin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.visualvm.remotevm;
 
@@ -8,12 +19,9 @@ import com.sun.tools.visualvm.core.datasource.DataSource;
 import com.sun.tools.visualvm.core.datasource.descriptor.DataSourceDescriptor;
 import com.sun.tools.visualvm.core.datasource.descriptor.DataSourceDescriptorFactory;
 import com.sun.tools.visualvm.core.model.AbstractModelProvider;
-import com.sun.tools.visualvm.host.RemoteHostsContainer;
-import com.sun.tools.visualvm.host.RemoteHostsContainerDescriptor;
 
 /**
- *
- * @author ragoale
+ * @author Alexey Ragozin (alexey.ragozin@gmail.com)
  */
 public class RemoteSshHostsContainer extends DataSource {
 
--- visualvm-workbench/remote-vm-root/src/netbeans/src/org/visualvm/remotevm/SshHostCustomizer.java
+++ visualvm-workbench/remote-vm-root/src/netbeans/src/org/visualvm/remotevm/SshHostCustomizer.java
@@ -1,6 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2012-2014 Alexey Ragozin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.visualvm.remotevm;
 
@@ -37,11 +48,12 @@ import com.sun.tools.visualvm.core.properties.PropertiesSupport;
 import com.sun.tools.visualvm.core.ui.components.ScrollableContainer;
 
 /**
- * 
- * @author ragoale
+ * @author Alexey Ragozin (alexey.ragozin@gmail.com)
  */
 public class SshHostCustomizer extends JPanel {
 
+	private static final long serialVersionUID = 20140112L;
+	
 	private static Dimension MIN_PROPERTIES_SIZE = new Dimension(400, 200);
 	private static Dimension MAX_PROPERTIES_SIZE = new Dimension(700, 400);
 
