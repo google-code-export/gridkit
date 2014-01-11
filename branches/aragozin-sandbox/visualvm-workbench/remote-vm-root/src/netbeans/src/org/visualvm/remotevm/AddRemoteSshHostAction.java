101ef9882f83f2d182eb99536ff7a0867ba5d7d4
alexey.ragozin@gmail.com <alexey.ragozin@gmail.com@4e96dbfe-9890-11de-ac5d-73b9d092dc0a>
2014-01-11 23:15:52 +0000

    [remotevm] Minor cleanup

--- branches/aragozin-sandbox/visualvm-workbench/remote-vm-root/src/netbeans/src/org/visualvm/remotevm/AddRemoteSshHostAction.java
+++ branches/aragozin-sandbox/visualvm-workbench/remote-vm-root/src/netbeans/src/org/visualvm/remotevm/AddRemoteSshHostAction.java
@@ -1,6 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2012-2014 Alexey Ragozin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.visualvm.remotevm;
 
@@ -14,12 +25,13 @@ import org.openide.util.NbBundle;
 import org.openide.util.RequestProcessor;
 
 /**
- *
- * @author ragoale
+ * @author Alexey Ragozin (alexey.ragozin@gmail.com)
  */
 public class AddRemoteSshHostAction extends SingleDataSourceAction<RemoteSshHostsContainer> {
     
-    private static final String ICON_PATH = "com/sun/tools/visualvm/host/resources/addRemoteHost.png";  // NOI18N
+	private static final long serialVersionUID = 20140112L;
+	
+	private static final String ICON_PATH = "com/sun/tools/visualvm/host/resources/addRemoteHost.png";  // NOI18N
     private static final Image ICON =  ImageUtilities.loadImage(ICON_PATH);
     
     private boolean tracksSelection = false;
@@ -45,7 +57,6 @@ public class AddRemoteSshHostAction extends SingleDataSourceAction<RemoteSshHost
         return selectionAware;
     }
     
-    
     protected void actionPerformed(RemoteSshHostsContainer remoteHostsContainer, ActionEvent actionEvent) {
         final SshHostProperties hostDescriptor = SshHostCustomizer.defineHost();
         if (hostDescriptor != null) {
@@ -64,7 +75,6 @@ public class AddRemoteSshHostAction extends SingleDataSourceAction<RemoteSshHost
     protected void updateState(Set<RemoteSshHostsContainer> remoteHostsContainerSet) {
         if (tracksSelection) super.updateState(remoteHostsContainerSet);
     }
-    
     
     private AddRemoteSshHostAction() {
         super(RemoteSshHostsContainer.class);
